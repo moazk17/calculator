4edc93d7c2fd9493ce952e2d0716391f837b74f0
Moazk127 <mkgmahra@uwaterloo.ca>
2023-03-05 20:16:14 -0500

    implemented calculator functionality

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let result = 0;
+let firstNumber;
+let secondNumber;
+let operator;
+let input = '';
+
 function add(x, y) {
     return x+y;
 }
@@ -24,8 +30,55 @@ function operate(operator, x, y) {
         case '/':
             return divide(x, y);
             break;
-        default:
-            return 'Error';
-            break;
     }
 }
+
+let display = document.querySelector('.displayInput');
+
+let buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if (e.target.classList.contains('number')) {
+            if (display.textContent == '0') {
+                display.textContent = '';
+            }
+            input += e.target.textContent;
+            display.textContent =input;
+        }
+        if (e.target.classList.contains('operator')) {
+            secondNumber = input;
+            input = '';
+            if (firstNumber) {
+                let result = operate(operator, Number(firstNumber), Number(secondNumber));
+                display.textContent = result;
+                firstNumber = result;
+                secondNumber = '';
+                operator = e.target.textContent;
+            }
+            else {
+                firstNumber = display.textContent;
+                secondNumber = '';
+                operator = e.target.textContent;
+                display.textContent = '0';
+            }
+        }
+        if (e.target.classList.contains('equal')) {
+            secondNumber = display.textContent;
+            let result = operate(operator, Number(firstNumber), Number(secondNumber));
+             display.textContent = result;
+            firstNumber = result;
+            secondNumber = '';
+             operator = '';
+             
+            
+        }
+        if (e.target.classList.contains('clear')) {
+            display.textContent = '0';
+            firstNumber = '';
+            secondNumber = '';
+            operator = '';
+            input = '';
+        }
+
+    })
+});
